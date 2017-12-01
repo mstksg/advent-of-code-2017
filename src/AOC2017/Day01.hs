62a45fb6dfa95a05fe8d4af9d260bf3c4a36dd83
Justin Le <justin@jle.im>
2017-12-01 08:12:15 -0800

    bisect using foldl

--- src/AOC2017/Day01.hs
+++ src/AOC2017/Day01.hs
@@ -1,22 +1,22 @@
 module AOC2017.Day01 (day01a, day01b) where
 
 import           AOC2017.Types
+import           Data.Bifunctor
 import           Data.Char
-import qualified Data.Sequence as Seq
-import           Data.Sequence (Seq(..), (|>))
 import           Data.Foldable
+import           Data.Sequence  (Seq(..), (|>))
+import qualified Data.Sequence  as Seq
 
 conseqs :: [a] -> [(a,a)]
 conseqs []     = []
 conseqs (x:xs) = zip (x:xs) (xs ++ [x])
 
 bisect :: [a] -> ([a],[a])
-bisect = go False [] Seq.empty
+bisect = bimap reverse toList . snd . foldl' go (False, ([], Seq.empty))
   where
-    go _     ys zs         []     = (reverse ys, toList zs)
-    go False ys zs         (x:xs) = go True  ys     (zs |> x        ) xs
-    go True  ys Empty      (x:xs) = go False ys     (Seq.singleton x) xs
-    go True  ys (z :<| zs) (x:xs) = go False (z:ys) (zs |> x        ) xs
+    go (False, (ys, zs      )) x = (True , (ys  , zs |> x        ))
+    go (True , (ys, z :<| zs)) x = (False, (z:ys, zs |> x        ))
+    go (True , (ys, Empty   )) x = (False, (ys  , Seq.singleton x))
 
 matchings :: Eq a => [(a,a)] -> [a]
 matchings = map fst . filter (uncurry (==))
