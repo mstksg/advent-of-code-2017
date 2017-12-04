52a1900312221afe7704b992ad56813513e08f82
Justin Le <justin@jle.im>
2017-12-04 14:33:34 -0800

    simpler day02, but is now unsafe

--- src/AOC2017/Day02.hs
+++ src/AOC2017/Day02.hs
@@ -4,7 +4,6 @@ module AOC2017.Day02 (day02a, day02b) where
 
 import           AOC2017.Types (Challenge)
 import           Data.List     (sort, tails)
-import           Data.Maybe    (listToMaybe)
 
 parse :: String -> [[Int]]
 parse = map (map (read @Int) . words) . lines
@@ -15,10 +14,10 @@ day02a = show . sum . map check . parse
     check xs = maximum xs - minimum xs
 
 day02b :: Challenge
-day02b = maybe "Bad lines" (show . sum) . traverse (check . sort) . parse
+day02b = show . sum . map (check . sort) . parse
   where
     -- a bit of prolog never hurt anyone?
-    check xs = listToMaybe $ do
+    check xs = head $ do
       y:ys      <- tails xs
       candidate <- ys
       (d, 0)    <- return $ candidate `divMod` y
