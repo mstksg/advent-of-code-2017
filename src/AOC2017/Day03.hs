0fb9078ad01ca9a0491a99cb0f961b616a125d56
Justin Le <justin@jle.im>
2017-12-05 14:13:04 -0800

    day 03 rewritten to directly use Trail type instead of EndoKleisli Writer

--- src/AOC2017/Day03.hs
+++ src/AOC2017/Day03.hs
@@ -1,34 +1,35 @@
 
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns  #-}
 
 module AOC2017.Day03 (day03a, day03b) where
 
 import           AOC2017.Types              (Challenge)
-import           Control.Monad              ((>=>))
 import           Control.Monad.Trans.State  (State, state, evalState)
-import           Control.Monad.Trans.Writer (Writer, writer, execWriter)
 import           Data.List                  (find)
 import           Data.Maybe                 (mapMaybe, fromJust)
 import           Data.Semigroup             (Semigroup(..), Sum(..))
 import qualified Data.Map                   as M
 
--- Why aren't these defined in base by default?
-data EndoKleisli m a = EK { runEK :: a -> m a }
-instance Monad m => Semigroup (EndoKleisli m a) where
-    f <> g = EK (runEK f >=> runEK g)
-instance Monad m => Monoid (EndoKleisli m a) where
-    mempty  = EK return
+-- | Monoid that composes functions, and aggregates logs that the functions
+-- emit
+newtype Trail a = Trail { runTrail :: a -> ([a], a) }
+instance Semigroup (Trail a) where
+    f <> g = Trail $ \x -> let (xs, y) = runTrail f x
+                               (ys, z) = runTrail g y
+                           in  (xs ++ ys, z)
+instance Monoid (Trail a) where
+    mempty  = Trail ([],)
     mappend = (<>)
 
-type Trail a = EndoKleisli (Writer [a]) a
-
+-- | 'Sum' just so I can add two points together with '(<>)'
 type Point = (Sum Int, Sum Int)
 
 norm :: Point -> Int
 norm (Sum x, Sum y) = abs x + abs y
 
 move :: Point -> Trail Point
-move p = EK $ \p0 -> writer (p0 <> p, [p0 <> p])
+move p = Trail $ \p0 -> ([p0 <> p], p0 <> p)
 
 spiral :: Trail Point
 spiral = move (0,0)
@@ -41,7 +42,7 @@ spiral = move (0,0)
           <> stimes (2*n  ) (move ( 0,-1))
 
 ulam :: [Point]
-ulam = execWriter $ runEK spiral (0,0)
+ulam = fst $ runTrail spiral (0,0)
 
 day03a :: Challenge
 day03a (read->i) = show . norm $ ulam !! (i - 1)
