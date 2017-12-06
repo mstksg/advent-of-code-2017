360aaa99fa0925a4b0ccd1e33f874a4a951fbf66
Justin Le <justin@jle.im>
2017-12-05 22:36:55 -0800

    rewriting with some lens

--- src/AOC2017/Day06.hs
+++ src/AOC2017/Day06.hs
@@ -1,30 +1,32 @@
 module AOC2017.Day06 (day06a, day06b) where
 
 import           AOC2017.Types  (Challenge)
+import           Control.Lens   (set, over, ix, (.~), (+~))
 import           Data.Bifunctor
+import           Data.Function  ((&))
 import           Data.Maybe
 import           Data.Tuple
 import qualified Data.IntMap    as IM
 import qualified Data.Map       as M
+import qualified Data.Vector    as V
 
 dropBlocks
     :: Int                  -- ^ number to drop
     -> Int                  -- ^ index to start dropping at
-    -> IM.IntMap Int
-    -> IM.IntMap Int
-dropBlocks 0 _ m0 = m0
-dropBlocks n i m0 = dropBlocks (n - 1) i' . IM.adjust (+ 1) i' $ m0
+    -> V.Vector Int
+    -> V.Vector Int
+dropBlocks 0 _ v = v
+dropBlocks n i v = dropBlocks (n - 1) i' v'
   where
-    i' = (i + 1) `mod` (fst (IM.findMax m0) + 1)
+    i' = (i + 1) `mod` V.length v
+    v' = v & ix i' +~ 1
 
-step :: IM.IntMap Int -> IM.IntMap Int
-step m0 = dropBlocks maxBlocks maxIx $ IM.insert maxIx 0 m0
+step :: V.Vector Int -> V.Vector Int
+step v = dropBlocks maxBlocks maxIx v'
   where
-    (maxBlocks, maxIx) = second negate
-                       . maximum
-                       . map (second negate . swap)
-                       . IM.toList
-                       $ m0
+    maxIx     = V.maxIndex v
+    maxBlocks = v V.! maxIx
+    v'        = v & ix maxIx .~ 0
 
 -- | Returns the location of the first loop, and the length of the loop
 findLoop :: Ord a => [a] -> Maybe (Int, Int)
@@ -37,8 +39,8 @@ findLoop = go 0 M.empty
                  in  go (n + 1) (M.insert x 1 m') xs
 
 day06 :: String -> (Int, Int)
-day06 = fromJust . findLoop . iterate step . IM.fromList
-      . zip [0..] . map read . words
+day06 = fromJust . findLoop . iterate step
+      . V.fromList . map read . words
 
 day06a :: Challenge
 day06a = show . fst . day06
