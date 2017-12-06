0d6a21a98d4f1c1c9299f52f98bc44d50ff210e8
Justin Le <justin@jle.im>
2017-12-05 22:54:29 -0800

    working with vectors, getting rid of explicit recursion when possible

--- src/AOC2017/Day06.hs
+++ src/AOC2017/Day06.hs
@@ -1,25 +1,24 @@
+{-# LANGUAGE TupleSections #-}
+
 module AOC2017.Day06 (day06a, day06b) where
 
-import           AOC2017.Types  (Challenge)
-import           Control.Lens   (set, over, ix, (.~), (+~))
-import           Data.Bifunctor
-import           Data.Function  ((&))
+import           AOC2017.Types (Challenge)
+import           Control.Lens  (ix, (.~), (+~))
+import           Data.Function ((&))
 import           Data.Maybe
-import           Data.Tuple
-import qualified Data.IntMap    as IM
-import qualified Data.Map       as M
-import qualified Data.Vector    as V
+import qualified Data.Map      as M
+import qualified Data.Vector   as V
 
 dropBlocks
     :: Int                  -- ^ number to drop
     -> Int                  -- ^ index to start dropping at
     -> V.Vector Int
     -> V.Vector Int
-dropBlocks 0 _ v = v
-dropBlocks n i v = dropBlocks (n - 1) i' v'
+dropBlocks n i0 v0 = snd $ iterate dropBlock (i0, v0) !! n
   where
-    i' = (i + 1) `mod` V.length v
-    v' = v & ix i' +~ 1
+    dropBlock (i, v) = (i', v & ix i' +~ 1)
+      where
+        i' = (i + 1) `mod` V.length v
 
 step :: V.Vector Int -> V.Vector Int
 step v = dropBlocks maxBlocks maxIx v'
@@ -34,9 +33,10 @@ findLoop = go 0 M.empty
   where
     go _ _ []     = Nothing
     go n m (x:xs) = case M.lookup x m of
-      Just l  -> Just (n, l)
-      Nothing -> let m' = (+1) <$> m
-                 in  go (n + 1) (M.insert x 1 m') xs
+        Just l  -> Just (n, l)
+        Nothing -> go (n + 1) (M.insert x 1 m') xs
+      where
+        m' = succ <$> m
 
 day06 :: String -> (Int, Int)
 day06 = fromJust . findLoop . iterate step
