9d9dda76df440883fdeb98bd70143d3d672cc7c0
Justin Le <justin@jle.im>
2017-12-08 23:42:56 -0800

    even cleaner Day 9?

--- src/AOC2017/Day09.hs
+++ src/AOC2017/Day09.hs
@@ -4,11 +4,13 @@
 module AOC2017.Day09 (day09a, day09b) where
 
 import           AOC2017.Types              (Challenge)
-import           Control.Monad.Trans.Class
-import           Control.Monad.Trans.Writer
-import           Data.Either
-import           Data.Monoid
-import           Data.Void
+import           Control.Monad.Trans.Class  (lift)
+import           Control.Monad.Trans.Writer (Writer, runWriter, tell)
+import           Data.Bifunctor             (bimap)
+import           Data.Either                (fromRight)
+import           Data.Functor               (($>))
+import           Data.Monoid                (Sum(..))
+import           Data.Void                  (Void)
 import qualified Text.Megaparsec            as P
 import qualified Text.Megaparsec.Char       as P
 
@@ -33,7 +35,7 @@ parseTree = P.choice [ Group   <$> parseGroup
       where
         inGarbage :: Parser ()
         inGarbage = P.choice
-          [ () <$ P.char '>'
+          [ P.char '>' $> ()
           , P.char '!' *> P.anyChar     *> inGarbage
           , P.anyChar  *> lift (tell 1) *> inGarbage
           ]
@@ -45,13 +47,13 @@ treeScore = go 1
       Garbage  -> 0
       Group ts -> n + sum (go (n + 1) <$> ts)
 
+parse :: String -> (Tree, Int)
+parse = bimap (fromRight undefined) getSum
+      . runWriter
+      . P.runParserT parseTree ""
+
 day09a :: Challenge
-day09a = show . treeScore
-       . fromRight undefined . fst . runWriter
-       . P.runParserT parseTree ""
+day09a = show . treeScore . fst . parse
 
 day09b :: Challenge
-day09b = show . getSum
-       . execWriter
-       . P.runParserT parseTree ""
-
+day09b = show .       snd       . parse
