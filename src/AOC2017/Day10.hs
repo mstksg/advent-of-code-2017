b581f858855d120b4d8887b266a81c7ff47526cd
Justin Le <justin@jle.im>
2017-12-10 00:23:41 -0800

    day 10 cleanup, import list

--- src/AOC2017/Day10.hs
+++ src/AOC2017/Day10.hs
@@ -5,14 +5,11 @@
 module AOC2017.Day10 (day10a, day10b) where
 
 import           AOC2017.Types   (Challenge)
-import           Control.Monad
-import           Data.Bits
-import           Data.Char
-import           Data.List
-import           Data.List.Split
-import           Data.Word
-import           Debug.Trace
-import           Text.Printf
+import           Data.Bits       (xor)
+import           Data.Char       (ord)
+import           Data.List       (foldl')
+import           Data.List.Split (chunksOf, splitOn)
+import           Text.Printf     (printf)
 import qualified Data.Text       as T
 import qualified Data.Vector     as V
 
@@ -27,9 +24,9 @@ initHS = HS (V.generate 256 id) 0 0
 step :: HashState -> Int -> HashState
 step (HS v0 p0 s0) n = HS v1 p1 s1
   where
-    ixes = (`mod` V.length v0) <$> [ p0 .. p0 + n - 1 ]
+    ixes = (`mod` 256) <$> [ p0 .. p0 + n - 1 ]
     v1 = v0 V.// zip ixes ((v0 V.!) <$> reverse ixes)
-    p1 = (p0 + n + s0) `mod` V.length v0
+    p1 = (p0 + n + s0) `mod` 256
     s1 = s0 + 1
 
 day10a :: Challenge
