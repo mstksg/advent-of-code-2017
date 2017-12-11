ae6b074fcaea878249965dedb5a3d8a576298f7d
Justin Le <justin@jle.im>
2017-12-10 22:36:09 -0800

    anticipating Day 12

--- src/AOC2017.hs
+++ src/AOC2017.hs
@@ -4,19 +4,20 @@ module AOC2017 (
   , challengeMap
   ) where
 
-import AOC2017.Day01 as AOC
-import AOC2017.Day02 as AOC
-import AOC2017.Day03 as AOC
-import AOC2017.Day04 as AOC
-import AOC2017.Day05 as AOC
-import AOC2017.Day06 as AOC
-import AOC2017.Day07 as AOC
-import AOC2017.Day08 as AOC
-import AOC2017.Day09 as AOC
-import AOC2017.Day10 as AOC
-import AOC2017.Day11 as AOC
-
+import           AOC2017.Day01 as AOC
+import           AOC2017.Day02 as AOC
+import           AOC2017.Day03 as AOC
+import           AOC2017.Day04 as AOC
+import           AOC2017.Day05 as AOC
+import           AOC2017.Day06 as AOC
+import           AOC2017.Day07 as AOC
+import           AOC2017.Day08 as AOC
+import           AOC2017.Day09 as AOC
+import           AOC2017.Day10 as AOC
+import           AOC2017.Day11 as AOC
+import           AOC2017.Day12 as AOC
 import           AOC2017.Types as AOC
+
 import qualified Data.IntMap   as IM
 import qualified Data.Map      as M
 
@@ -44,5 +45,7 @@ challengeMap = IM.fromList
                       ,('b', day10b)])
     , (11, M.fromList [('a', day11a)
                       ,('b', day11b)])
+    , (12, M.fromList [('a', day12a)
+                      ,('b', day12b)])
     ]
 
---
+++ src/AOC2017/Day12.hs
@@ -0,0 +1,9 @@
+module AOC2017.Day12 (day12a, day12b) where
+
+import           AOC2017.Types (Challenge)
+
+day12a :: Challenge
+day12a = undefined
+
+day12b :: Challenge
+day12b = undefined
