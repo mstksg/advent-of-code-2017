1055d53a77a135e08801a21bbc48bacdf6dd8529
Justin Le <justin@jle.im>
2017-12-15 22:19:49 -0800

    initial day 16

--- src/AOC2017/Day15.hs
+++ src/AOC2017/Day15.hs
@@ -1,9 +1,25 @@
 module AOC2017.Day15 (day15a, day15b) where
 
-import           AOC2017.Types        (Challenge)
+import           AOC2017.Types (Challenge)
+import           Data.Char
 
-day15a :: Challenge
+generate :: Int -> Int -> Int
+generate fac = go
+  where
+    go = (`mod` 2147483647) . (* fac) 
+
+facA, facB :: Int
+facA = 16807
+facB = 48271
+
 day15a = undefined
+-- day15a :: Challenge
+-- day15a (map grab.lines->a:b:_) = show . length . filter p $ zip (iterate (generate facA) a)
+--                                                                 (iterate (generate facB) b)
+--   where
+--     p (x,y) = x `mod` 
+
+-- grab = read . filter isDigit
 
 day15b :: Challenge
 day15b = undefined
--- src/AOC2017/Day16.hs
+++ src/AOC2017/Day16.hs
@@ -1,10 +1,54 @@
+{-# LANGUAGE BangPatterns #-}
+
 module AOC2017.Day16 (day16a, day16b) where
 
-import           AOC2017.Types        (Challenge)
+import           AOC2017.Types   (Challenge)
+import           Data.List
+import           Data.List.Split
+import qualified Data.Vector     as V
+
+data Move = MSpin     Int
+          | MExchange Int  Int
+          | MPartner  Char Char
+
+step :: V.Vector Char -> Move -> V.Vector Char
+step v0 = \case
+  MSpin n       -> let (x,y) = V.splitAt (16 - n) v0
+                   in  y V.++ x
+  MExchange n m -> v0 V.// [(n, v0 V.! m),(m, v0 V.! n)]
+  MPartner n m  -> let Just iN = V.findIndex (== n) v0
+                       Just iM = V.findIndex (== m) v0
+                   in  v0 V.// [(iN, m),(iM, n)]
+
+parse :: String -> [Move]
+parse = map parseMove . splitOn ","
+  where
+    parseMove :: String -> Move
+    parseMove = \case
+      's':(read->n)            -> MSpin n
+      'x':(splitOn "/"->n:m:_) -> MExchange (read n) (read m)
+      'p':n:_:m:_              -> MPartner n m
+      _                        -> error "No parse"
+
+initial :: V.Vector Char
+initial = V.fromList ['a' .. 'p']
 
 day16a :: Challenge
-day16a = undefined
+day16a = V.toList
+       . foldl' step initial
+       . parse
 
 day16b :: Challenge
-day16b = undefined
-
+day16b (parse->moves) = V.toList
+                      . foldl' step initial
+                      . concat
+                      $ replicate leftovers moves
+  where
+    oneCycle xs = foldl' step xs moves
+    loopLength = (+ 1)
+               . length
+               . takeWhile (/= initial)
+               . drop 1
+               . iterate oneCycle
+               $ initial
+    leftovers = 1000000000 `mod` loopLength
