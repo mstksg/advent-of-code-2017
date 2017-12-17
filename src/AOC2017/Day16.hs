e8c8edb6e66272eacce7ebbe17c8b967ee97ead2
Justin Le <justin@jle.im>
2017-12-16 16:34:43 -0800

    refactored day 16 using direct product and dual

--- src/AOC2017/Day16.hs
+++ src/AOC2017/Day16.hs
@@ -3,45 +3,42 @@ module AOC2017.Day16 (day16a, day16b) where
 import           AOC2017.Types   (Challenge)
 import           Data.Char       (chr, ord)
 import           Data.List.Split (splitOn)
-import           Data.Semigroup  (Semigroup(..), stimes)
+import           Data.Semigroup  (Semigroup(..), stimes, Dual(..))
 import qualified Data.Map        as M
 
-data Dance = DP { _dPos  :: !(M.Map Int Int)
-                , _dName :: !(M.Map Char Char)
-                }
-  deriving Show
+newtype Perm a = P { permMap :: M.Map a a }
+    deriving Show
 
-instance Semigroup Dance where
-    DP p1 n1 <> DP p2 n2 = DP (p1 `comp` p2) (n2 `comp` n1)
-      where
-        comp m1 m2 = (follow <$> m2) `M.union` m1
-          where
-            follow i = M.findWithDefault i i m1
-instance Monoid Dance where
+lookupPerm :: Ord a => Perm a -> a -> a
+lookupPerm p k = M.findWithDefault k k (permMap p)
+
+instance Ord a => Semigroup (Perm a) where
+    x <> y = P $ (lookupPerm x <$> permMap y) `M.union` permMap x
+instance Ord a => Monoid (Perm a) where
     mappend = (<>)
-    mempty = DP M.empty M.empty
+    mempty  = P M.empty
 
+type Dance = (Perm Int, Dual (Perm Char))
 
 runDance :: Dance -> String
-runDance d = lookupPerm (_dName d)
-           . toName
-           . lookupPerm (_dPos d)
-         <$> [0..15]
+runDance (pI, pN) = lookupPerm (getDual pN)
+                  . toName
+                  . lookupPerm pI
+                <$> [0..15]
   where
-    lookupPerm m k = M.findWithDefault k k m
-    toName c       = chr (c + ord 'a')
+    toName c = chr (c + ord 'a')
 
 parseMove :: String -> Dance
 parseMove = \case
-    's':(read->n)                     -> DP (rotator n)   M.empty
-    'x':(map read.splitOn "/"->n:m:_) -> DP (swapper n m) M.empty
-    'p':n:_:m:_                       -> DP M.empty       (swapper n m)
+    's':(read->n)                     -> (rotator n  , mempty            )
+    'x':(map read.splitOn "/"->n:m:_) -> (swapper n m, mempty            )
+    'p':n:_:m:_                       -> (mempty     , Dual (swapper n m))
     _                                 -> error "No parse"
   where
-    rotator :: Int -> M.Map Int Int
-    rotator n = M.fromList [ (i, (i - n) `mod` 16) | i <- [0..15] ]
-    swapper :: Ord a => a -> a -> M.Map a a
-    swapper x y = M.fromList [ (x, y), (y, x) ]
+    rotator :: Int -> Perm Int
+    rotator n = P $ M.fromList [ (i, (i - n) `mod` 16) | i <- [0..15] ]
+    swapper :: Ord a => a -> a -> Perm a
+    swapper x y = P $ M.fromList [ (x, y), (y, x) ]
 
 parse :: String -> Dance
 parse = foldMap parseMove . splitOn ","
