9b844ee3824d9800efc34523d34823d58f406205
Justin Le <justin@jle.im>
2017-12-19 15:36:54 -0800

    cleaned up extra inBounds validation by using safe gridAt and pattern matching

--- src/AOC2017/Day19.hs
+++ src/AOC2017/Day19.hs
@@ -1,7 +1,7 @@
 module AOC2017.Day19 (day19a, day19b) where
 
 import           AOC2017.Types             (Challenge)
-import           Control.Applicative       (many)
+import           Control.Applicative       (many, empty)
 import           Control.Monad             (guard)
 import           Control.Monad.Trans.Class (lift)
 import           Control.Monad.Trans.State (StateT, evalStateT, get, put)
@@ -16,25 +16,23 @@ type Point = L.V2 Int
 follow :: Grid -> StateT (Point, Point) [] Char
 follow g = do
     -- (last position, current position)
-    (x0, x1) <- get
-    x2 <- lift $ case gridAt x1 of
-        ' ' -> []
-        '+' -> (+ x1) <$> [ L.V2 0    1
-                          , L.V2 0    (-1)
-                          , L.V2 1    0
-                          , L.V2 (-1) 0
-                          ]
-        _   -> [ x1 + (x1 - x0) ]
-    let nextChar = gridAt x2
+    (x0, x1)      <- get
+    Just currChar <- return $ gridAt x1
+    x2 <- case currChar of
+        ' ' -> empty
+        '+' -> (+ x1) <$> lift [ L.V2 0    1
+                               , L.V2 0    (-1)
+                               , L.V2 1    0
+                               , L.V2 (-1) 0
+                               ]
+        _   -> return $ x1 + (x1 - x0)
+    Just nextChar <- return $ gridAt x2
     guard $ x2 /= x0
-    guard $ inBounds x2
     guard $ nextChar `elem` "|-+" || isAlpha nextChar
     put (x1, x2)
     return nextChar
   where
-    gridAt   (L.V2 x y) = g V.! y V.! x
-    inBounds (L.V2 x y) = all ($ x) [(>= 0), (< V.length (g V.! 0))]
-                       && all ($ y) [(>= 0), (< V.length g        )]
+    gridAt   (L.V2 x y) = (V.!? x) =<< g V.!? y
 
 -- | Repeat search many times until a dead end is found, using 'many'
 followToTheEnd :: Grid -> StateT (Point, Point) [] String
