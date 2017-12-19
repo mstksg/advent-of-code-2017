f92dad8199c5113dc33186e0cebfe4a587169d13
Justin Le <justin@jle.im>
2017-12-19 15:06:37 -0800

    day 19 using StateT list and many

--- src/AOC2017/Day19.hs
+++ src/AOC2017/Day19.hs
@@ -1,43 +1,40 @@
 module AOC2017.Day19 (day19a, day19b) where
 
 import           AOC2017.Types             (Challenge)
-import           Control.Applicative
-import           Control.Monad
-import           Control.Monad.Trans.State
-import           Data.Char
-import           Data.Maybe
+import           Control.Applicative       (many)
+import           Control.Monad             (guard)
+import           Control.Monad.Trans.Class (lift)
+import           Control.Monad.Trans.State (StateT, evalStateT, get, put)
+import           Data.Char                 (isAlpha)
 import qualified Data.Vector               as V
 import qualified Linear                    as L
 
-type Grid = V.Vector (V.Vector Char)
+type Grid  = V.Vector (V.Vector Char)
 type Point = L.V2 Int
 
-follow :: Grid -> StateT (L.V2 Point) Maybe Char
+follow :: Grid -> StateT (L.V2 Point) [] Char
 follow g = do
-    L.V2 x0 (x1@(L.V2 x y)) <- get
-    let nextSteps = case g V.! y V.! x of
-          '+' -> fmap (,'+') . listToMaybe $ search x0 x1
-          ' ' -> Nothing
-          c   -> Just (x1 + (x1 - x0), c)
-    case nextSteps of
-      Nothing        -> empty
-      Just (x2, out) -> out <$ put (L.V2 x1 x2)
+    L.V2 x0 x1 <- get
+    x2 <- lift $ case gridAt x1 of
+        ' ' -> []
+        '+' -> (+ x1) <$> [ L.V2 0    1
+                          , L.V2 0    (-1)
+                          , L.V2 1    0
+                          , L.V2 (-1) 0
+                          ]
+        _   -> [ x1 + (x1 - x0) ]
+    guard $ x2 /= x0 && inBounds x2
+    let nextChar = gridAt x2
+    guard $ (nextChar `elem` "|-+") || isAlpha nextChar
+    put (L.V2 x1 x2)
+    return nextChar
   where
-    search x0 x1 = do
-      p@(L.V2 x y) <- (+ x1) <$> [ L.V2 0    1
-                                 , L.V2 0    (-1)
-                                 , L.V2 1    0
-                                 , L.V2 (-1) 0
-                                 ]
-      guard $ p /= x0 && valid p
-      let c = g V.! y V.! x
-      guard $ (c `elem` "|-") || isAlpha c
-      return p
-    valid (L.V2 x y) = all ($ x) [(>= 0), (< V.length (g V.! 0))]
-                    && all ($ y) [(>= 0), (< V.length g        )]
+    gridAt   (L.V2 x y) = g V.! y V.! x
+    inBounds (L.V2 x y) = all ($ x) [(>= 0), (< V.length (g V.! 0))]
+                       && all ($ y) [(>= 0), (< V.length g        )]
 
 day19 :: Grid -> String
-day19 g = fromJust . flip evalStateT p0 . many . follow $ g
+day19 g = ('|':) . head . flip evalStateT p0 . many . follow $ g
   where
     p0 = L.V2 (L.V2 x0 (-1)) (L.V2 x0 0)
     Just x0 = V.elemIndex '|' (g V.! 0)
