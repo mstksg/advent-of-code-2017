a4b17f404343dcefeeac8423922a7308ba9b0b05
Justin Le <justin@jle.im>
2017-12-25 03:14:14 -0800

    day 25 partially rewritten to use parser instead of hard-coded values

--- src/AOC2017/Day25.hs
+++ src/AOC2017/Day25.hs
@@ -1,13 +1,18 @@
 module AOC2017.Day25 (day25a) where
 
-import           AOC2017.Types   (Challenge)
-import           AOC2017.Util    ((!!!))
-import           Control.Lens.At (Contains(..))
-import           Data.Bifunctor  (first)
-import           Data.Coerce     (coerce)
-import           Data.Semigroup  (Semigroup(..), Last(..), Sum(..))
-import qualified Data.IntSet     as IS
-import qualified Data.Map        as M
+import           AOC2017.Types              (Challenge)
+import           AOC2017.Util               ((!!!))
+import           Control.Applicative        (Applicative(..), Alternative(..))
+import           Control.Lens.At            (Contains(..))
+import           Control.Monad              (replicateM)
+import           Data.Bifunctor             (first)
+import           Data.Semigroup             (Semigroup(..), Last(..), Sum(..))
+import           Data.Void                  (Void)
+import qualified Data.IntSet                as IS
+import qualified Data.Map                   as M
+import qualified Text.Megaparsec            as P
+import qualified Text.Megaparsec.Char       as P
+import qualified Text.Megaparsec.Char.Lexer as PL
 
 type St      = Last Char
 type Step    = (Sum Int, St)
@@ -21,22 +26,48 @@ runRule rm st = \case
 step :: RuleMap -> Step -> IS.IntSet -> (Step, IS.IntSet)
 step rm s0@(Sum i,st) = first (s0 <>) . contains i (runRule rm st)
 
-
 day25a :: Challenge
-day25a _ = show . IS.size . snd
-         . (!!! 12964419)
-         . iterate (uncurry (step ruleMap))
-         $ ((0, Last 'A'), IS.empty)
+day25a (parse->RS{..}) = show . IS.size . snd . (!!! rsSteps)
+                       . iterate (uncurry (step rsRuleMap))
+                       $ ((0, rsStart), IS.empty)
 
-ruleMap :: RuleMap
-ruleMap = M.fromList (coerce ruleList)
+
+
+-- Ugly parser stuff below
+
+data RuleSet = RS { rsStart   :: St
+                  , rsSteps   :: Int
+                  , rsRuleMap :: RuleMap
+                  }
+  deriving Show
+
+type Parser = P.Parsec Void String
+
+parse :: String -> RuleSet
+parse = either (error . P.parseErrorPretty) id . P.runParser ruleSet ""
+
+ruleSet :: Parser RuleSet
+ruleSet = do
+    st0 <- parseLine "Begin in state"                      P.anyChar
+    stp <- parseLine "Perform a diagnostic checksum after" PL.decimal
+    rules <- many rule
+    return RS { rsStart   = Last st0
+              , rsSteps   = stp
+              , rsRuleMap = M.fromList rules
+              }
   where
-    ruleList :: [(Char, (((Int, Char), Bool), ((Int, Char), Bool)))]
-    ruleList = [ ('A', ((( 1, 'B'), True ), (( 1, 'F'), False)))
-               , ('B', (((-1, 'B'), False), ((-1, 'C'), True )))
-               , ('C', (((-1, 'D'), True ), (( 1, 'C'), False)))
-               , ('D', (((-1, 'E'), True ), (( 1, 'A'), True )))
-               , ('E', (((-1, 'F'), True ), ((-1, 'D'), False)))
-               , ('F', ((( 1, 'A'), True ), ((-1, 'E'), False)))
-               ]
-
+    rule :: Parser (Last Char, ((Step, Bool), (Step, Bool)))
+    rule = do
+      st <- parseLine "In state" P.anyChar
+      [r0,r1] <- replicateM 2 $ do
+        _ <- parseLine "If the current value is" P.anyChar
+        b <- (== '1')
+         <$> parseLine "- Write the value"       P.anyChar
+        d <- (\case "left" -> -1; _ -> 1)
+         <$> parseLine "- Move one slot to the"  (P.many P.letterChar)
+        c <- parseLine "- Continue with state"   P.anyChar
+        return ((Sum d, Last c), b)
+      return (Last st, (r0, r1))
+    parseLine :: String -> Parser a -> Parser a
+    parseLine str = P.between (P.space *> P.string str *> P.space)
+                              (P.skipManyTill P.anyChar P.newline)
